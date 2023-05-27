6d0401b21916dd8dbbfe9d2d875920e044cecb9e
cpprian <cpprian456@gmail.com>
2023-05-27 21:00:33 +0200

    update branch

--- users/cmd/app/helpers.go
+++ users/cmd/app/helpers.go
@@ -6,10 +6,6 @@ import (
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
-	app.errorLog.Println("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Printf("%s\n%s", err.Error(), debug.Stack())
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-}
-
-func (app *application) clientError(w http.ResponseWriter, status int) {
-	http.Error(w, http.StatusText(status), status)
 }
